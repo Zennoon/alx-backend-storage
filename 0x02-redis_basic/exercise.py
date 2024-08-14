72ff6b578d2dd579296afaca3aae7b7d0892463a
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-08-14 23:57:44 +0000

    Write decorators to store stats about method calls

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -8,6 +8,25 @@ Contains:
 import redis
 import uuid
 from typing import Any, Awaitable, Callable, Type, Union
+from functools import wraps
+
+
+def call_history(method):
+    @wraps(method)
+    def record_method_io(self, *args):
+        self._redis.rpush("{}:inputs".format(method.__qualname__), str(args))
+        output = method(self, *args)
+        self._redis.rpush("{}:outputs".format(method.__qualname__), output)
+        return output
+    return record_method_io
+
+
+def count_calls(method):
+    @wraps(method)
+    def incr_call_counter(self, *args, **kwargs):
+        self._redis.incr(method.__qualname__, 1)
+        return method(self, *args, **kwargs)
+    return incr_call_counter
 
 
 class Cache:
@@ -17,6 +36,8 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
+    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """
         Stores data to the client with a random key
@@ -59,3 +80,30 @@ class Cache:
         passed as lambda callback
         """
         return self.get(key, lambda val: val.decode("utf-8"))
+
+
+def replay(method):
+    """
+    Replays the history of a method of the Cache class
+    """
+    conn = redis.Redis()
+    method_name = method.__qualname__
+    count = conn.get(method_name)
+    if count:
+        count = count.decode("utf-8")
+
+    print("{} was called {} times:".format(method_name, count))
+    inputs = conn.lrange(
+        "{}:inputs".format(method_name), 0, -1
+    )
+    outputs = conn.lrange(
+        "{}:outputs".format(method_name), 0, -1
+    )
+    zipped = zip(inputs, outputs)
+
+    for inp, outp in zipped:
+        print("{}(*{}) -> {}".format(
+            method_name,
+            inp.decode("utf-8"),
+            outp.decode("utf-8")
+        ))
