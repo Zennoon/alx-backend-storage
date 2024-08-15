f74029dd92d62f1408a06e62c10728fba94b83d3
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-08-15 01:29:30 +0000

    Try error handling

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -22,11 +22,15 @@ def get_page(url: str) -> str:
     """Receives a url and retrieves the decoded (utf-8) content"""
     if not url or len(url.strip()) == 0:
         return ''
-    text = conn.get(url).decode('utf-8')
+    text = conn.get(url)
     count_key = "count:{}".format(url)
     conn.incr(count_key, 1)
     if not text:
-        text = requests.get(url).text
-        conn.set(url, text)
-        conn.expire(url, timedelta(seconds=10))
-    return text
+        try:
+            text = requests.get(url).text
+        except Exception:
+            return ''
+        else:
+            conn.set(url, text)
+            conn.expire(url, timedelta(seconds=10))
+    return text.decode('utf-8')
