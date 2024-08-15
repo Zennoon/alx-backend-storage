38774f4f7ce5dc7e2164f084684ef3d7d5c56409
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-08-15 01:33:54 +0000

    Try initializing count to 0

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -16,22 +16,32 @@ import json
 import redis
 import requests
 
-conn = redis.Redis()
+redis_store = redis.Redis()
+'''The module-level Redis instance.
+'''
 
 
+def data_cacher(method: Callable) -> Callable:
+    '''Caches the output of fetched data.
+    '''
+    @wraps(method)
+    def invoker(url) -> str:
+        '''The wrapper function for caching the output.
+        '''
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
+        result = method(url)
+        redis_store.set(f'count:{url}', 0)
+        redis_store.setex(f'result:{url}', 10, result)
+        return result
+    return invoker
+
+
+@data_cacher
 def get_page(url: str) -> str:
-    """Receives a url and retrieves the decoded (utf-8) content"""
-    if not url or len(url.strip()) == 0:
-        return ''
-    text = conn.get(url)
-    count_key = "count:{}".format(url)
-    conn.incr(count_key, 1)
-    if not text:
-        try:
-            text = requests.get(url).text
-        except Exception:
-            return ''
-        else:
-            conn.set(url, json.dumps(text))
-            conn.expire(url, timedelta(seconds=10))
-    return str(json.loads(text.decode('utf-8')))
+    '''Returns the content of a URL after caching the request's response,
+    and tracking the request.
+    '''
+    return requests.get(url).text
