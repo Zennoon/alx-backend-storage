c4adea8bb23015aa567889431a7b548d6aa140fa
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-08-15 01:01:35 +0000

    Try using a pipeline

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -18,15 +18,16 @@ import requests
 
 def get_page(url: str) -> str:
     """Receives a url and retrieves the decoded (utf-8) content"""
-    if url is None or len(url.strip()) == 0:
-        return ''
-    redis_store = redis.Redis()
-    res_key = 'result:{}'.format(url)
-    req_key = 'count:{}'.format(url)
-    result = redis_store.get(res_key)
-    if result is not None:
-        redis_store.incr(req_key)
-        return result
-    result = requests.get(url).content.decode('utf-8')
-    redis_store.setex(res_key, timedelta(seconds=10), result)
-    return result
+    conn = redis.Redis()
+    try:
+        text = requests.get(url).text
+    except Exception:
+        pass
+    else:
+        count_key = "count:{}".format(url)
+        with conn.pipeline() as pipe:
+            pipe.incr(count_key, 1)
+            pipe.expire(count_key, timedelta(seconds=10))
+            pipe.execute()
+        return text
+    return text
