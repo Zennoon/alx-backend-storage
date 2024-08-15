e25602bf46a6978271fc9287ffc520e05d6e86b0
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-08-15 00:37:00 +0000

    Try without decorators

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -13,28 +13,12 @@ Contains:
 """
 import redis
 import requests
-from functools import wraps
-from typing import Callable
 
 
-def count_url(func: Callable) -> Callable:
-    """Tracks the urls passed to the get_page function"""
-    conn = redis.Redis()
-
-    @wraps(func)
-    def incr_count(url: str):
-        """
-        Increments the count of the url in the redis server by 1
-        and sets an expiration date of 10 seconds for the count
-        """
-        key = "count:{}".format(url)
-        conn.incr(key, 1)
-        conn.expire(key, 10)
-        return (func(url))
-    return incr_count
-
-
-@count_url
 def get_page(url: str) -> str:
     """Receives a url and retrieves the decoded (utf-8) content"""
+    conn = redis.Redis()
+    key = "count:{}".format(url)
+    conn.incr(key, 1)
+    conn.expire(key, 10)
     return requests.get(url).text
