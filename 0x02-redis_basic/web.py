b1951fcd0b93a01314010cbfa7c9841fdf666087
Yunus Kedir <98653096+Zennoon@users.noreply.github.com>
2024-08-16 18:04:25 +0000

    Reimplement function and first flesh out counter only

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -5,34 +5,20 @@ Contains:
     =========
     get_page - Receives a url and returns the html content
 
-    Decorators
-    ==========
-    count_url - Wraps get_page and keeps count of the number of
-    times a url has been requested (given to the function), and
-    caches the result for 10 seconds
 """
-from datetime import timedelta
-import json
 import redis
 import requests
+from datetime import timedelta
 
 conn = redis.Redis()
 
 
 def get_page(url: str) -> str:
-    """Receives a url and retrieves the decoded (utf-8) content"""
-    if not url or len(url.strip()) == 0:
-        return ''
-    text = conn.get(url)
-    count_key = "count:{}".format(url)
-    conn.incr(count_key, 1)
-    if not text:
-        try:
-            text = requests.get(url).content
-        except Exception:
-            return ''
-        else:
-            print(text)
-            conn.set(url, text)
-            conn.expire(url, timedelta(seconds=10))
-    return text.decode('utf-8')
+    """Receives a url and retrieves the HTML content"""
+    conn.incr("count:{}".format(url), 1)
+    text = ''
+    try:
+        text = requests.get(url).text
+    except Exception:
+        text = ''
+    return text
